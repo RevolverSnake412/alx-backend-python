dcfc2b752f8717795d44d48aa62d19713c468fe0
RevolverSnake412 <oussag@hotmail.fr>
2024-04-19 00:59:18 +0000

    update

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,4 @@
-#!/usr/env python3
+#!/usr/bin/env python3
 '''
 type-annotated function that takes
 a float a and b and returns their sum
@@ -6,4 +6,7 @@ a float a and b and returns their sum
 
 
 def add(a: float, b: float):
+    '''
+    returns the sum of taken args
+    '''
     return a + b
